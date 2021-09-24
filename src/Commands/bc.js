30f246f37e8304ef0b3bc5a3633afdd904ed6491
Jet <jwoot001@gmail.com>
2021-09-24 15:53:48 +0800

    restore token prices

--- src/Commands/bc.js
+++ src/Commands/bc.js
@@ -14,49 +14,53 @@ module.exports = new Command({
 		/*
 		Table Config
 		*/
-		var data = [
-			["", "Breed Count 1", "Breed Count 2", "Breed Count 3", "Breed Count 4", "Breed Count 5", "Breed Count 6", "Breed Count 7"],          
-			["SLP", "600", "1500", "3000", "5400", "9300", "15600", "25800"],
-			["AXS", "1", "2", "3", "4", "5", "6", "7"]
-		];
-
-		const config = {
-			columns: [
-			  { alignment: 'center' },
-			  { alignment: 'center' },
-			  { alignment: 'center' },
-			  { alignment: 'center' },
-			  { alignment: 'center' },
-			  { alignment: 'center' },
-			  { alignment: 'center' },
-			  { alignment: 'center' }
-			],
-		  };
-
-		/*
-		Execute command
-		*/
-		
-		if(args.length <= 1) {
+		if(args.length < 1) {
+			var data = [
+				["", "Breed Count 1", "Breed Count 2", "Breed Count 3", "Breed Count 4", "Breed Count 5", "Breed Count 6", "Breed Count 7"],          
+				["SLP", "600", "1500", "3000", "5400", "9300", "15600", "25800"],
+				["AXS", "1", "2", "3", "4", "5", "6", "7"]
+			];
+	
+			const config = {
+				columns: [
+				  { alignment: 'center' },
+				  { alignment: 'center' },
+				  { alignment: 'center' },
+				  { alignment: 'center' },
+				  { alignment: 'center' },
+				  { alignment: 'center' },
+				  { alignment: 'center' },
+				  { alignment: 'center' }
+				],
+			  };
+	
+			/*
+			Execute command
+			*/
 			
-			//get token data
-			let dtToken = await tokenModel.find({ 
-				tokenID: { $in: ['smooth-love-potion',	'axie-infinity']}
-			});
-
-			//axs
-			let axsprc = dtToken[0].php;
-
-			//slp
-			let slpprc = dtToken[1].php;
-			let ttlArr = ['PHP'];
-
-			for(let i = 1; i < 8; i++) {
-				ttlArr[i] = fix.toDecimal((slpprc * data[1][i]) + (axsprc * data[2][i]))
+			if(args.length <= 1) {
+				
+				//get token data
+				let dtToken = await tokenModel.find({ 
+					tokenID: { $in: ['smooth-love-potion',	'axie-infinity']}
+				});
+	
+				//axs
+				let axsprc = dtToken[0].php;
+	
+				//slp
+				let slpprc = dtToken[1].php;
+				let ttlArr = ['PHP'];
+	
+				for(let i = 1; i < 8; i++) {
+					ttlArr[i] = fix.toDecimal((slpprc * data[1][i]) + (axsprc * data[2][i]))
+				}
+	
+				data[3] = ttlArr;
+				message.channel.send(`\`\`\`cs\n${table(data, config)}\`\`\``)
 			}
-
-			data[3] = ttlArr;
-			message.channel.send(`\`\`\`cs\n${table(data, config)}\`\`\``)
+		} else {
+			message.channel.send('Error: Invalid command. `!bc` to show breeding costs.')
 		}
 		
 	}
