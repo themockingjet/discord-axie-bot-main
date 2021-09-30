373793f26e6d936f9bda3d7c01f62e26bfd5561d
Jet <jwoot001@gmail.com>
2021-09-30 12:44:35 +0800

    dc webhook

---
+++ discord_web.js
@@ -0,0 +1,12 @@
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook("https://discord.com/api/webhooks/892511642548592680/IAXDHpl1VKtmrbeb9ZaRbram6tFYCLPqkPMZJ9GeCT7--wsJOsy_WV0HJ21rr9khZrDe");
+
+(async () => {
+    try {
+        await hook.send('Restarting bot...');
+        console.log('Successfully sent webhook!');
+    }
+    catch(e){
+        console.log(e.message);
+    };
+})();
--- restart_node.sh
+++ restart_node.sh
@@ -5,6 +5,21 @@ git pull
 
 echo ""
 echo "=================================="
+echo "Restarting bot..."
+node webhook.js
+
+echo ""
+echo "=================================="
+echo "Installing npm packages"
+pm2 stop 1
+
+echo ""
+echo "=================================="
+echo "Installing npm packages"
+npm install
+
+echo ""
+echo "=================================="
 echo "Restarting node...\n"
 pm2 restart 1
 
