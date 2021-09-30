d7d91604243e158f46b91845d3da80a255457717
Jet <jwoot001@gmail.com>
2021-09-30 13:02:19 +0800

    peek

--- restart_node.sh
+++ restart_node.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
-echo "Pulling files from the repo."
-git pull
-
 echo ""
-echo "=================================="
+echo "=========================================================="
 echo "Restarting bot..."
 node discord_web.js
 
+echo "Pulling files from the repo."
+echo "=========================================================="
+git pull
+
+
 echo ""
-echo "=================================="
+echo "=========================================================="
 echo "Installing npm packages"
 pm2 stop 1
 
 echo ""
-echo "=================================="
+echo "=========================================================="
 echo "Installing npm packages"
 npm install
 
 echo ""
-echo "=================================="
+echo "=========================================================="
 echo "Restarting node...\n"
 pm2 restart 1
 
 echo ""
-echo "=================================="
+echo "=========================================================="
 echo "Exiting..."
 exit 0
--- src/Commands/peek.js
+++ src/Commands/peek.js
@@ -3,11 +3,13 @@
 //
 const Command = require("../Structures/Command.js");
 const fetch = require('node-fetch');
-const { MessageEmbed } = require("discord.js");
-const convert = require('ether-converter')
+const { MessageAttachment,MessageEmbed } = require("discord.js");
+const convert = require('ether-converter');
+const nodeHtmlToImage = require('node-html-to-image');
 
 module.exports = new Command({
 	name: "peek",
+	aliases: ["p"],
 	description: "Peek axie genes.",
 	async run(message, args, client, member) {
 
@@ -19,7 +21,105 @@ module.exports = new Command({
 			minute: 60 * 1000,
 			second: 1000
 		}
-
+		var htmlCode = `<!DOCTYPE html>
+		<html dir="ltr" lang="en" class="">
+		<head>
+			<meta charset="utf-8">
+			<style>
+				body { 
+					background-color: transparent;
+				}
+				div.a {
+					display: inline-block;
+					width: 300px;
+					height: 300px;
+				}
+				
+				div.b {
+					display: inline-block;
+					width: 400px;
+					height: 300px;
+				}
+				th {
+					color: #000000;
+				}
+				th, td {
+					font-family: verdana;
+					font-size: 20px;
+					font-weight: bold;
+					padding: 3px;
+				}
+				#aquatic {
+					color:#01B1C7;
+				}
+				#beast {
+					color:#FFB812;
+				}
+				#bug {
+					color:#B4433C;
+				}
+				#reptile {
+					color:#C88AE0;
+				}
+				#plant {
+					color:#60A808;
+				}
+				#bird {
+					color:#FF8BBD;
+				}
+				table > 
+			</style>
+		</head>
+		<body>
+		<div class="a">
+			<img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/5345871/axie/axie-full-transparent.png" 
+				 alt=""
+				 style="height: 100%; width: 100%; object-fit: contain;">
+		</div>
+		<div class="b">
+			<table>
+				<tr>
+				  <th>D</th>
+				  <th>R1</th>
+				  <th>R2</th>
+				</tr>
+				<tr>
+					<td id="{{ceD}}">{{eyesD}}</td>
+					<td id="{{ceR1}}">{{eyesR1}}</td>
+					<td id="{{ceR2}}">{{eyesR2}}</td>
+				</tr>
+				<tr>
+					<td id="{{ceaD}}">{{earsR1}}</td>
+					<td id="{{ceaR1}}">{{earsR2}}</td>
+					<td id="{{ceaR2}}">{{earsD}}</td>
+				</tr>
+				<tr>
+					<td id="{{cmD}}">{{mouthD}}</td>
+					<td id="{{cmR1}}">{{mouthR1}}</td>
+					<td id="{{cmR2}}">{{mouthR2}}</td>
+				</tr>
+				<tr>
+					<td id="{{chD}}">{{hornD}}</td>
+					<td id="{{chR1}}">{{hornR1}}</td>
+					<td id="{{chR2}}">{{hornR2}}</td>
+				</tr>
+				<tr>
+					<td id="{{cbD}}">{{backD}}</td>
+					<td id="{{cbR1}}">{{backR1}}</td>
+					<td id="{{cbR2}}">{{backR2}}</td>
+				</tr>
+				<tr>
+					<td id="{{ctD}}">{{tailD}}</td>
+					<td id="{{ctR1}}">{{tailR1}}</td>
+					<td id="{{ctR2}}">{{tailR2}}</td>
+				</tr>
+			</table>
+		</div>
+			
+		
+		
+		</body>
+		</html>`
 		var rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
 
 		var getRelativeTime = (d1, d2 = new Date()) => {
@@ -163,7 +263,7 @@ module.exports = new Command({
 				__typename
 			  }`;
 
-			const axGraph = await fetch('https://graphql-gateway.axieinfinity.com/graphql', {
+			const axGraph = await fetch('https://axieinfinity.com/graphql-server-v2/graphql', {
 									method: 'POST',
 									headers: {
 									'Content-Type': 'application/json',
@@ -176,9 +276,61 @@ module.exports = new Command({
 										},
 										query
 									})
-									})
+								})
 								.then(r => r.json())
-			
+								// ${axieGene.eyes.d.name} [${axieGene.eyes.d.class.substr(0,1)}]
+			// const geneImg = router.get(`/api/tweet/render`, async function(req, res) {
+			// 	const image = await nodeHtmlToImage({
+			// 	html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
+			// 	});
+			// 	res.writeHead(200, { 'Content-Type': 'image/png' });
+			// 	res.end(image, 'binary');
+			// });
+
+			const geneImg = await nodeHtmlToImage({
+					html: htmlCode,
+					type: 'png',
+					transparent: true,
+					content: {
+						eyesD: axieGene.eyes.d.name,
+						ceD: axieGene.eyes.d.class,
+						eyesR1: axieGene.eyes.r1.name,
+						ceR1: axieGene.eyes.r1.class,
+						eyesR2: axieGene.eyes.r2.name,
+						ceR2: axieGene.eyes.r2.class,
+						earsD: axieGene.ears.d.name,
+						ceaD: axieGene.ears.d.class,
+						earsR1: axieGene.ears.r1.name,
+						ceaR1: axieGene.ears.r1.class,
+						earsR2: axieGene.ears.r2.name,
+						ceaR2: axieGene.ears.r2.class,
+						mouthD: axieGene.mouth.d.name,
+						cmD: axieGene.mouth.d.class,
+						mouthR1: axieGene.mouth.r1.name,
+						cmR1: axieGene.mouth.r1.class,
+						mouthR2: axieGene.mouth.r2.name,
+						cmR2: axieGene.mouth.r2.class,
+						hornD: axieGene.horn.d.name,
+						chD: axieGene.horn.d.class,
+						hornR1: axieGene.horn.r1.name,
+						chR1: axieGene.horn.r1.class,
+						hornR2: axieGene.horn.r2.name,
+						chR2: axieGene.horn.r2.class,
+						backD: axieGene.eyes.d.name,
+						cbD: axieGene.eyes.d.class,
+						backR1: axieGene.eyes.r1.name,
+						cbR1: axieGene.eyes.r1.class,
+						backR2: axieGene.eyes.r2.name,
+						cbR2: axieGene.eyes.r2.class,
+						tailD: axieGene.tail.d.name,
+						ctD: axieGene.tail.d.class,
+						tailR1: axieGene.tail.r1.name,
+						ctR1: axieGene.tail.r1.class,
+						tailR2: axieGene.tail.r2.name,
+						ctR2: axieGene.tail.r2.class,
+					}
+				})
+			const file = new MessageAttachment(geneImg, 'axie.png')
 			//embed
 			const embed = new MessageEmbed()
 				.setColor('#FFFFFF')
@@ -203,72 +355,10 @@ module.exports = new Command({
 			}
 
 			embed
-				.addFields(
-					
-					// Stats
-					//{ name: 'Stats', value: `**Health**: ${axieStats.stats.hp}     **Speed**: ${axieStats.stats.speed}     **Skill**: ${axieStats.stats.skill}     **Morale**: ${axieStats.stats.morale}`}
-					{ 
-						name: 'D', 
-						value: `${axieGene.eyes.d.name} [${axieGene.eyes.d.class.substr(0,1)}]
-								${axieGene.ears.d.name} [${axieGene.ears.d.class.substr(0,1)}]
-								${axieGene.mouth.d.name} [${axieGene.mouth.d.class.substr(0,1)}]
-								${axieGene.horn.d.name} [${axieGene.horn.d.class.substr(0,1)}]
-								${axieGene.back.d.name} [${axieGene.back.d.class.substr(0,1)}]
-								${axieGene.tail.d.name} [${axieGene.tail.d.class.substr(0,1)}]`, 
-						inline: true },
-					{ 
-						name: 'R1', 
-						value: `${axieGene.eyes.r1.name} [${axieGene.eyes.r1.class.substr(0,1)}]
-								${axieGene.ears.r1.name} [${axieGene.ears.r1.class.substr(0,1)}]
-								${axieGene.mouth.r1.name} [${axieGene.mouth.r1.class.substr(0,1)}]
-								${axieGene.horn.r1.name} [${axieGene.horn.r1.class.substr(0,1)}]
-								${axieGene.back.r1.name} [${axieGene.back.r1.class.substr(0,1)}]
-								${axieGene.tail.r1.name} [${axieGene.tail.r1.class.substr(0,1)}]`, 
-						inline: true },
-					{ 
-						name: 'R2', 
-						value: `${axieGene.eyes.r2.name} [${axieGene.eyes.r2.class.substr(0,1)}]
-								${axieGene.ears.r2.name} [${axieGene.ears.r2.class.substr(0,1)}]
-								${axieGene.mouth.r2.name} [${axieGene.mouth.r2.class.substr(0,1)}]
-								${axieGene.horn.r2.name} [${axieGene.horn.r2.class.substr(0,1)}]
-								${axieGene.back.r2.name} [${axieGene.back.r2.class.substr(0,1)}]
-								${axieGene.tail.r2.name} [${axieGene.tail.r2.class.substr(0,1)}]`, 
-						inline: true },
-				)
-				.setImage(`https://storage.googleapis.com/assets.axieinfinity.com/axies/${args[1]}/axie/axie-full-transparent.png`)
+				.setImage('attachment://axie.png')
 				.setTimestamp()
-			
-			// if (!args[2] && args[2] == 'genes') {
-			// 	//add for 2nd arguments 'genes'
-			// }
-			message.channel.send({ embeds: [embed] });
+
+			message.channel.send({ embeds: [embed], files: [ file ]});
 		}
-
-
-
-		// if (isNaN(parseInt(args[1]))) {
-		// 	return message.channel.send("Invalid amount. Type `!axs <amount>` or `!axs <amount> @ <price>`.")
-
-		// } else if (args[2] === "@" && !args[3]) {
-		// 	return message.channel.send("Invalid command. Type `!axs <amount> <@> <price>`.")
-
-		// } else if (args[2] === "@" && !isNaN(parseInt(args[3]))){
-			
-		// 	let y = parseFloat(args[3]).toFixed(2);
-		// 	let x = (y * args[1]).toFixed(2);
-
-		// 	message.reply(`\`AXS\` **${args[1]}** => \`PHP\` **${x}**`);
-			
-		// } else if (!isNaN(parseInt(args[1])) && !args[2]) {
-			
-		// 	fetch('https://api.coingecko.com/api/v3/simple/price?ids=axie-infinity&vs_currencies=php')
-		// 	.then(res => res.json())
-		// 	.then(body => {
-		// 		let x = body['axie-infinity'].php.toFixed(2) * args[1];
-		// 		message.reply(`\`AXS\` **${args[1]}** => \`PHP\` **${x}**`);
-		// 	});
-		// } else {
-		// 	return message.channel.send("Invalid command. Type `!axs <amount>` or `!axs <amount> @ <price>`.")
-		// };
 	}
 });
